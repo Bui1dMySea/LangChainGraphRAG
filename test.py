2d1e33d06e9d3e5f4e7ceb3d7c882c43e844bd5d
lwj <20224227039@suda.stu.edu.cn>
2024-11-29 10:14:38 +0800

    Misc:删除一些无用文件

--- test.py
+++
@@ -1,9 +0,0 @@
-test = ("{{\n"
-    "  \"head\": \"John Doe\",\n"
-    "  \"head_type\": \"person\",\n"
-    "  \"tail\": \"John Doe\",\n"
-    "  \"tail_type\": \"University of Example\",\n"
-    "  \"relation\": \"PROFESSOR\"\n"
-    "}}\n"
-)
-print(test)
